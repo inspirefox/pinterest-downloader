34c72598b8995b5464d6b70d90549ba0f54268e2
inspirefox <154806703+inspirefox@users.noreply.github.com>
2026-04-26 13:14:39 +0530

    Update server.js
    grg

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const axios = require("axios");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-// 👉 Yaha apni API key daalo
-const API_KEY = "b4346b269cmshef0a29e27076f9fp11fc67jsnad80e88c7996";
-
-// 👉 API host (RapidAPI se milega)
-const API_HOST = "pinterest-video-and-image-downloader.p.rapidapi.com";
-
 // Home Page
 app.get("/", (req, res) => {
   res.send(`
@@ -27,49 +21,34 @@ app.post("/download", async (req, res) => {
   const url = req.body.url;
 
   try {
-    const response = await axios.get(
-      "https://pinterest-video-and-image-downloader.p.rapidapi.com/",
-      {
-        params: { url },
-        headers: {
-          "X-RapidAPI-Key": API_KEY,
-          "X-RapidAPI-Host": API_HOST
-        }
-      }
-    );
+    // 🔥 unofficial API (no key needed)
+    const api = `https://pinterestdownloader.io/api/pinterest?url=${encodeURIComponent(url)}`;
 
+    const response = await axios.get(api);
     const data = response.data;
 
-    // 🔥 Video
-    if (data.data && data.data.videos && data.data.videos.length > 0) {
-      const videoUrl = data.data.videos[0].url;
-
+    if (data && data.video) {
       res.send(`
         <h2>Video Found 🎥</h2>
-        <video src="${videoUrl}" controls width="400"></video>
+        <video src="${data.video}" controls width="400"></video>
         <br><br>
-        <a href="${videoUrl}" download>Download Video</a>
+        <a href="${data.video}" download>Download Video</a>
       `);
-    }
-
-    // 🖼️ Image
-    else if (data.data && data.data.images && data.data.images.length > 0) {
-      const imgUrl = data.data.images[0].url;
-
+    } 
+    else if (data && data.image) {
       res.send(`
         <h2>Image Found 🖼️</h2>
-        <img src="${imgUrl}" width="300"/>
+        <img src="${data.image}" width="300"/>
         <br><br>
-        <a href="${imgUrl}" download>Download Image</a>
+        <a href="${data.image}" download>Download Image</a>
       `);
-    }
-
+    } 
     else {
       res.send("❌ Media not found");
     }
 
   } catch (err) {
-    res.send("⚠️ API Error: " + err.message);
+    res.send("⚠️ Error: " + err.message);
   }
 });
 
